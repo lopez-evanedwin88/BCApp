2c1ebf420258dfacf0011e954bd3ccd48e163e4d
Evan Edwin Lopez <elopez@iripple.com>
2023-03-27 17:33:55 +0800

    Import images

---
+++ src/constants/Images.ts
@@ -0,0 +1,8 @@
+const Images = {
+  arrow_right: require('../assets/images/icons_arrow_right.png'),
+  email: require('../assets/images/icons_mail.png'),
+  contact: require('../assets/images/icons_phone.png'),
+  card_background: require('../assets/images/card_background.png'),
+};
+
+export default Images;
