5d1ef18211910ab585b531154ff31c3fb4cbc81e
Evan Edwin Lopez <elopez@iripple.com>
2023-03-28 17:56:37 +0800

    Added Image

--- src/constants/Images.ts
+++ src/constants/Images.ts
@@ -3,6 +3,10 @@ const Images = {
   email: require('../assets/images/icons_mail.png'),
   contact: require('../assets/images/icons_phone.png'),
   card_background: require('../assets/images/card_background.png'),
+  name: require('../assets/images/icons_name.png'),
+  occupation: require('../assets/images/icons_occupation.png'),
+  company: require('../assets/images/icons_company.png'),
+  linked_in: require('../assets/images/icons_linked_in.png'),
 };
 
 export default Images;
